3e01ab90276265d9dd0cdee83394e9d946867570
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-04 15:33:57 +0000

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str, strnlen(str, sizeof(str)));
+	return (1);
+}
