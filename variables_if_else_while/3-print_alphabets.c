466f3ee521c4ea71d9015d1962cf53f2fce5b031
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-05 17:39:11 +0000

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 97;
+	while (letter <= 122)
+	{
+		putchar(letter);
+		letter++;
+	}
+	letter = 65;
+	while (letter <= 90)
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
