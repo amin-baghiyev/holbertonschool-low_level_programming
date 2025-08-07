cf5b286f3f00959454015b9e542c3ab2ee54314a
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-07 21:22:43 +0000

    a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @first: addend
+ * @second: addend
+ *
+ * Return: sum of two numbers
+ */
+int add(int first, int second)
+{
+	return (first + second);
+}
