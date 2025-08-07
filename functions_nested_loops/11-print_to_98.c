2b2bfbe4dec0de8e79bceb5a0cdb23b2ea04a493
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-07 21:33:18 +0000

    98 Battery Street, the OG

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: integer for starting point
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	while(n != 98)
+	{
+		if (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		else if (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	printf("%d\n", n);
+}
