d2df24f95dc987d8f521675a6ff823d389752949
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-09 18:34:47 +0000

    Even Liber Abbaci

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned int curr, prev, sum;
+
+	sum = 2;
+	prev = 1;
+	curr = 2;
+	while (curr < 4000000)
+	{
+		curr = curr + prev;
+		prev = curr - prev;
+		if (curr % 2 == 0)
+			sum += curr;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
