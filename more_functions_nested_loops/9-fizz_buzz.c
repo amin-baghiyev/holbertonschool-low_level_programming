a05dcfd759bb62c52ff2f6311b5afeabddf43e7a
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-09 16:09:43 +0000

    Fizz-Buzz - last space removed

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,13 +13,16 @@ int main(void)
 	while (n <= 100)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (n % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (n % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%hu ", n);
+			printf("%hu", n);
+
+		if (n != 100)
+			printf(" ");
 		n++;
 	}
 	printf("\n");
