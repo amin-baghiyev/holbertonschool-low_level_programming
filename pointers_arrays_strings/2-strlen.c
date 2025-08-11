02bf46c6b2aadff5756c84077e97c6f50df65ad3
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-11 21:44:08 +0000

    This report, by its very length, defends itself against the risk of being read

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - compute the length of a string
+ * @s: string to get length
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	unsigned long long length;
+
+	length = 0;
+	while (s[length] != '\0')
+		length++;
+
+	return (length);
+}
