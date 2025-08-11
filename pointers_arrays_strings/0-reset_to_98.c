5bde9d6295a499a54106f300f232b4aeeeb9ae42
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-11 18:01:10 +0000

    98 Battery st.

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes parameter and updates the value it to 98
+ * @n: integer value that will be updated to 98
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
