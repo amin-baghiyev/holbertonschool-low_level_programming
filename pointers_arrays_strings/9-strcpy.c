fec5e6b7df373a7ae6e3dc7d966bf6e27d3d93ee
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-11 23:27:21 +0000

    strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string pointed to by src to buffer pointed to by dest
+ * @dest: buffet to be written
+ * @src: string to copy
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	unsigned long i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[++i] = '\0';
+	dest = src;
+
+	return (dest);
+}
