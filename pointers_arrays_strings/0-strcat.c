69fe49d57396c44cc3b340425be841e14d29bc35
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-12 14:02:10 +0000

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to will be concatenate
+ * @src: string to cut
+ *
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	unsigned long i, j;
+	char flag;
+
+	i = 0;
+	j = 0;
+	flag = '0';
+	while (src[j] != '\0' || dest[i] != '\0')
+	{
+		if (dest[i] == '\0')
+			flag = '1';
+		if (flag == '1')
+		{
+			dest[i] = src[j];
+			j++;
+		}
+		i++;
+	}
+	return (dest);
+}
