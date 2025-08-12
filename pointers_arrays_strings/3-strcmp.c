d6008725244dfbae1b96a80eaad6e9be5dd4d3ca
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-12 17:17:59 +0000

    strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string to compare
+ * @s2: string to compare
+ *
+ * Return: an integer indicating which string is greater
+ */
+int *_strcmp(char *s1, char *s2)
+{
+	unsigned long i;
+
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+		i++;
+
+	return (s1[i] - s2[i]);
+}
