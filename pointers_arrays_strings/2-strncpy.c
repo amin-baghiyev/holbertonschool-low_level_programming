d4f6924ce62cf4f9cb1d099a9ca02b8474f3c05d
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-12 16:56:53 +0000

    strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies string pointed to by src to buffer pointed to by dest
+ * @dest: buffer to be written
+ * @src: string to copy
+ * @n: count of bytes
+ *
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; (src[i] != '\0' && i < n); i++)
+		dest[i] = src[i];
+	if (i < n)
+		dest[i] = '\0';
+	return (dest);
+}
