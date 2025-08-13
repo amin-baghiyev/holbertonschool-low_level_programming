8f35c7996a0960403931b077292f9881f2b78118
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-13 22:21:08 +0000

    Numbers have life; they're not just symbols on paper

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * power - compute result of raising a base to an exponent
+ * @base: base number
+ * @exponent: exponent to raise base to
+ *
+ * Return: value of base raised to the power of exponent
+ */
+int power(int base, int exponent)
+{
+	int result, i;
+
+	result = 1;
+	for (i = 0; i < exponent; i++)
+		result *= base;
+	return (result);
+}
+
+/**
+ * print_number - Prints an integer
+ * @n: integer to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int c;
+	int i;
+
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+	c = n;
+	i = -1;
+
+	while (n != 0)
+	{
+		n /= 10;
+		i++;
+	}
+
+	do {
+		_putchar(48 +  c / power(10, i));
+		c %= power(10, i);
+		i--;
+	} while (i >= 0);
+}
