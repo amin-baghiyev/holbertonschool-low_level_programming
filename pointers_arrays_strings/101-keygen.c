a3341349a13a7e5b0f4cc76b2ac3314513cda041
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-15 01:01:24 +0000

    Don't hate the hacker, hate the code

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void) {
+	srand(time(NULL));
+	unsigned short sum, c;
+
+	sum = 0;
+	while (sum < 2772)
+	{
+		c = 33 + rand() % 93;
+		if (sum + c > 2772)
+			c = 2772 - sum;
+		sum += c;
+		printf("%c", c);
+	}
+	return (0);
+}
