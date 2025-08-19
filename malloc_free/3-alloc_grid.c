d709b1591ec3b0d82236e05c9a44fc4df58d9eac
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-19 17:37:00 +0000

    If you even dream of beating me you'd better wake up and apologize - bugs fixed

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * alloc_grid - allocates 2 dimensional array of integers
@@ -9,8 +10,7 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **grid;
-	unsigned int i, j;
+	int **grid, i, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
