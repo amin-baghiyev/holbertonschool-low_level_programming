0b073e28beddb169ee034b87652481e67ee2369f
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-23 17:05:41 +0000

    We must accept finite disappointment, but never lose infinite hope - integer limit increased

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * main - Entry point
@@ -10,7 +11,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j;
+	char *num1, *num2;
+	int len1, len2, len, i, j, mul, sum, *result, start;
 
 	if (argc != 3)
 	{
@@ -26,7 +28,41 @@ int main(int argc, char *argv[])
 				exit(98);
 			}
 
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	num1 = argv[1], num2 = argv[2];
+	len1 = strlen(num1), len2 = strlen(num2);
+	len = len1 + len2;
+	result = malloc(sizeof(int) * len);
+
+	if (result == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (i = 0; i < len; i++)
+		result[i] = 0;
+
+	for (i = len1 - 1; i >= 0; i--)
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			mul = (num1[i] - '0') * (num2[j] - '0');
+			sum = mul + result[i + j + 1];
+			result[i + j + 1] = sum % 10;
+			result[i + j] += sum / 10;
+		}
+
+	start = 0;
+	while (start < len && result[start] == 0)
+		start++;
+
+	if (start == len)
+		putchar('0');
+	else
+		for (; start < len; start++)
+			putchar(result[start] + '0');
+	putchar('\n');
+
+	free(result);
 
 	return (0);
 }
