a15f5d2b03fce9ce8686f4f6d74afc3bf77d942b
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-25 20:17:35 +0000

    A door is what a dog is perpetually on the wrong side of

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - initializes new dog object
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * Return: pointer to newly created dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	unsigned int i;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	for (i = 0; name[i] != '\0'; i++)
+	;
+	dog->name = malloc(sizeof(char) * (i + 1));
+	if (dog->name == NULL)
+		return (NULL);
+	for (i = 0; name[i] != '\0'; i++)
+		dog->name[i] = name[i];
+	dog->name[i] = '\0';
+
+	dog->age = age;
+
+	for (i = 0; owner[i] != '\0'; i++)
+	;
+	dog->owner = malloc(sizeof(char) * (i + 1));
+	if (dog->owner == NULL)
+		return (NULL);
+	for (i = 0; owner[i] != '\0'; i++)
+		dog->owner[i] = owner[i];
+	dog->owner[i] = '\0';
+
+	return (dog);
+}
