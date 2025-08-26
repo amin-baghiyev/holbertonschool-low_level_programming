af706000d297e69cce7e10f7cb78f26162a45d6e
amin-baghiyev <baghiyev.amin@gmail.com>
2025-08-26 20:53:55 +0000

    A goal is not always meant to be reached, it often serves simply as something to aim at - operator bug fixed

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -15,10 +15,7 @@ int main(int argc, char *argv[])
 	int (*op)(int, int);
 
 	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+		printf("Error\n"), exit(98);
 
 	num1 = atoi(argv[1]), num2 = atoi(argv[3]);
 	op = get_op_func(argv[2]);
