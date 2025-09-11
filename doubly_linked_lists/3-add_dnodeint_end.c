46e3fb0daba3969446a4d685fab0b62bfcee2d80
amin-baghiyev <baghiyev.amin@gmail.com>
2025-09-11 21:11:04 +0000

    Add node at the end - prev bug fixed

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -17,14 +17,14 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (NULL);
 	element->n = n;
 	element->next = NULL;
-	element->prev = *head;
 	if (*head == NULL)
-		*head = element;
+		element->prev = NULL, *head = element;
 	else
 	{
 		h = *head;
 		while (h->next != NULL)
 			h = h->next;
+		element->prev = h;
 		h->next = element;
 	}
 	return (element);
