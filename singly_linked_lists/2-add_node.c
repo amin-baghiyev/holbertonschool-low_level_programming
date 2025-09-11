852800493372744d6c2e382db6e2b6315c50dc0d
amin-baghiyev <baghiyev.amin@gmail.com>
2025-09-11 15:18:57 +0000

    Add node - order fixed

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,9 +2,31 @@
 #include <string.h>
 #include <stdlib.h>
 
+/**
+ * _strlen - computes length of string
+ * @str: string
+ *
+ * Return: length of str
+ */
+int _strlen(const char *str)
+{
+	int i;
+
+	for(i = 0; str[i] != '\0'; i++)
+	;
+	return (i);
+}
+
+/**
+ * add_node - adds a new node at the beginning of linked list
+ * @head: linked list
+ * @str: string
+ *
+ * Return: address to new element
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *element, *h;
+	list_t *element;
 
 	element = malloc(sizeof(list_t));
 	if (element == NULL)
@@ -15,16 +37,8 @@ list_t *add_node(list_t **head, const char *str)
 		free(element);
 		return (NULL);
 	}
-	element->len = strlen(str);
-	element->next = NULL;
-	if (*head == NULL)
-		*head = element;
-	else
-	{
-		h = *head;
-		while (h->next != NULL)
-			h = h->next;
-		h->next = element;
-	}
+	element->len = _strlen(str);
+	element->next = *head;
+	*head = element;
 	return (element);
 }
