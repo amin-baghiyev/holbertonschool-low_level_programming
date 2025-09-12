9f77135a095245b59962f756d7188659f9185c38
amin-baghiyev <baghiyev.amin@gmail.com>
2025-09-12 21:32:44 +0000

    cp - read bug fixed i hope

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -70,16 +70,20 @@ int main(int argc, char *argv[])
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 		err_98(argv[1]);
+	r_bytes = read(fd_from, buffer, 1024);
+	if (r_bytes == -1)
+		 close_fd(fd_from), err_98(argv[1]);
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 		close_fd(fd_from), err_99(argv[2]);
-	while ((r_bytes = read(fd_from, buffer, 1024)) > 0)
+	while (r_bytes > 0)
 	{
-		if (r_bytes == -1)
-			close_fd(fd_from), close_fd(fd_to), err_98(argv[1]);
 		w_bytes = write(fd_to, buffer, r_bytes);
 		if (w_bytes != r_bytes)
 			close_fd(fd_from), close_fd(fd_to), err_99(argv[2]);
+		r_bytes = read(fd_from, buffer, 1024);
+		if (r_bytes == -1)
+			 close_fd(fd_from), close_fd(fd_to), err_98(argv[1]);
 	}
 	close_fd(fd_from), close_fd(fd_to);
 	return (0);
