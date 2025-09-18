abb331ff03b9945ccb1f37cd06ba10bbc5752374
amin-baghiyev <baghiyev.amin@gmail.com>
2025-09-18 21:04:09 +0000

    >>> ht['betty'] = 'cool' - bugs fixed

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,9 +15,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node;
 	unsigned long int i;
 
-	if (key[0] == '\0')
+	if (key == NULL)
 		return (0);
-	i = key_index((const unsigned char *)key, ht->size);
+	i = key_index((const unsigned char *)key, ht->size);	
+	node = ht->array[i];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value), node->value = strdup(value);
+			return (1);
+		}
+		node = node->next;
+	}
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
