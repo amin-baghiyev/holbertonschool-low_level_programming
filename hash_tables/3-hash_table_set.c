efbb927f4b2057f1412b15e8f7796386269df0d4
amin-baghiyev <baghiyev.amin@gmail.com>
2025-09-18 21:14:21 +0000

    >>> ht['betty'] = 'cool' - malloc is considered

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,15 +15,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node;
 	unsigned long int i;
 
-	if (key == NULL)
+	if (ht == NULL || key == NULL)
 		return (0);
-	i = key_index((const unsigned char *)key, ht->size);	
+	i = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[i];
 	while (node != NULL)
 	{
 		if (strcmp(node->key, key) == 0)
 		{
 			free(node->value), node->value = strdup(value);
+			if (node->value == NULL)
+				return (0);
 			return (1);
 		}
 		node = node->next;
@@ -31,8 +33,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
-	node->key = strdup(key), node->value = strdup(value);
-	node->next = ht->array[i];
-	ht->array[i] = node;
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (0);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key), free(node);
+		return (0);
+	}
+	node->next = ht->array[i], ht->array[i] = node;
 	return (1);
 }
